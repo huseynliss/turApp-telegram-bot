d22be9aa274c80e64125158ac8d636f7639de430
nijatfg <153588579+nijatfg@users.noreply.github.com>
2024-02-15 21:44:31 +0400

    offer deleted

--- src/main/java/az/code/turaltelegrambot/repository/OfferRepository.java
+++
@@ -1,8 +0,0 @@
-package az.code.turaltelegrambot.repository;
-
-
-import az.code.turaltelegrambot.entity.Offer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OfferRepository extends JpaRepository<Offer,Long> {
-}
