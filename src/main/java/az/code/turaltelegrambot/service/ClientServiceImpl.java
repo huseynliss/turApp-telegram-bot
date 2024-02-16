e380b51b5f9063ecab7bfd1fffa252eaf96ffac6
nazimfb <nazimfb@code.edu.az>
2024-02-16 14:15:35 +0400

    client service and repo

---
+++ src/main/java/az/code/turaltelegrambot/service/ClientServiceImpl.java
@@ -0,0 +1,38 @@
+package az.code.turaltelegrambot.service;
+
+import az.code.turaltelegrambot.entity.Client;
+import az.code.turaltelegrambot.repository.ClientRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClientServiceImpl implements ClientService {
+    private final ClientRepo clientRepo;
+    @Override
+    public Optional<Client> getByChatId(long chatId) {
+        return clientRepo.getClientByChatId(chatId);
+    }
+
+    @Override
+    public Optional<Client> getById(long id) {
+        return clientRepo.findById(id);
+    }
+
+    @Override
+    public Client create(Client client) {
+        return clientRepo.save(client);
+    }
+
+    @Override
+    public Client save(Client client) {
+        return clientRepo.save(client);
+    }
+
+    @Override
+    public void delete(long id) {
+        clientRepo.deleteById(id);
+    }
+}
