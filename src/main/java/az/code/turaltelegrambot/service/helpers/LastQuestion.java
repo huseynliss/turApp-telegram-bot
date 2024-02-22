4b44e5376f761a2202478251bfb255d9e8e9f29d
huseynlisahin <200320052003Baku>
2024-02-22 11:37:57 +0400

    add validations

--- src/main/java/az/code/turaltelegrambot/service/helpers/LastQuestion.java
+++ src/main/java/az/code/turaltelegrambot/service/helpers/LastQuestion.java
@@ -5,9 +5,9 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 
 public class LastQuestion {
     @Getter
-    private static long lastBotMessageChatId; // Store the chat ID of the last bot message
+    private static long lastBotMessageChatId;
     @Getter
-    private static Message lastBotMessage; // Store the last bot message
+    private static Message lastBotMessage;
 
     public static void setLastBotMessage(Message message) {
         lastBotMessage = message;
