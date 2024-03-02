0f57e383f63d5418631115f1e1ecd9f971f726d0
huseynlisahin <200320052003Baku>
2024-03-02 20:20:18 +0400

    add pagination for bot

---
+++ src/main/java/az/code/turaltelegrambot/entity/Offer.java
@@ -0,0 +1,31 @@
+package az.code.turaltelegrambot.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "offers")
+public class Offer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String price;
+    private String dateRange;
+    private String additionalInfo;
+    private UUID sessionId;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+}
+
